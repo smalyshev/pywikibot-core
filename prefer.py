bea9f44832393e72047f4de3fa40ca5c9b93e15c
Stanislav Malyshev <smalyshev@gmail.com>
2015-07-13 13:21:24 -0700

    add P102
    Change-Id: Ia6f6724b603fe1b426d692e1b483740705fdc481

--- prefer.py
+++ prefer.py
@@ -123,12 +123,13 @@ P6: head of government
 P17: country
 P35: head of state
 P36: capital
+P102: member of political party
 P176: manufacturer
 P488: chairperson
 P598: commander of
 """
 if not TEST:
-    props = [ 'P6', 'P17', 'P35', 'P36', 'P176',  'P488', 'P598']
+    props = [ 'P6', 'P17', 'P35', 'P36', 'P176',  'P488', 'P598', 'P26', 'P102']
 
 for prop in props:
     logpage = pywikibot.Page(site, LOGPAGE+prop)
