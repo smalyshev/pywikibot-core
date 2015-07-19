ac5944ec506895858296cf54d2dfd8b938d6403f
Stanislav Malyshev <smalyshev@gmail.com>
2015-07-19 16:35:03 -0700

    add p195
    Change-Id: I7ed2c8ba068d09caa027c1b404b84fc69aa09997

--- prefer.py
+++ prefer.py
@@ -131,11 +131,14 @@ P154: logo image
 P159: headquarters location
 P169: chief executive officer
 P176: manufacturer
+P195: collection
 P488: chairperson
 P598: commander of
 """
 if not TEST:
-    props = [ 'P6', 'P17', 'P35', 'P36', 'P94', 'P115', 'P118', 'P123', 'P137', 'P138', 'P154', 'P159', 'P169', 'P176', 'P488', 'P598',  ]
+    props = [ 'P6', 'P17', 'P35', 'P36', 'P94', 'P115', 'P118', 'P123', 'P137', 'P138', 'P154', 'P159', 'P169', 'P176', 'P195',
+             'P488', 'P598',
+    ]
 
 for prop in props:
     logpage = pywikibot.Page(site, LOGPAGE+prop)
