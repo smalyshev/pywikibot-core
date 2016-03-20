839eb38b58b3730b385bd5536d1fe075e605524f
Stanislav Malyshev <smalyshev@gmail.com>
2016-03-19 17:01:39 -0700

    Locator bot
    Change-Id: I6b5ebe30abeb4c84f1c2e8d65d7f001ab92ddbb3

---
+++ locator.py
@@ -0,0 +1,74 @@
+import pywikibot
+from pywikibot.data.sparql import SparqlQuery
+
+TEST = False
+
+ENTITY_PREFIX = 'http://www.wikidata.org/entity/'
+if TEST:
+    site = pywikibot.Site("test", "wikidata")
+    BODY = 'P734'
+    LOCATION = 'P125'
+else:
+    site = pywikibot.Site("wikidata", "wikidata")
+    BODY = 'P376'
+    LOCATION = 'P625'
+
+QUERY = """
+SELECT ?item ?body ?globe
+WHERE {
+?item wdt:P376 ?body .
+?item p:P625/psv:P625/wikibase:geoGlobe ?globe .
+filter (?globe != ?body) .
+} LIMIT 10
+"""
+
+sparql_query = SparqlQuery()
+repo = site.data_repository()
+globes = set(site.globes().values())
+
+def log_item(itemID, message):
+    print("[%s] %s" % (itemID, message))
+
+if TEST:
+    items = ['Q487']
+else:
+    items = sparql_query.get_items(QUERY, item_name='item')
+
+print(items)
+for itemID in items:
+    item = pywikibot.ItemPage(repo, itemID)
+    item.get()
+
+    if LOCATION not in item.claims:
+        continue
+
+    if BODY not in item.claims:
+        log_item(itemID, "Hmm, no body claim, weird...")
+        continue
+
+    if len(item.claims[BODY]) > 1:
+        log_item(itemID, "More than one body!")
+        continue
+
+    body = item.claims[BODY][0]
+    if body.getSnakType() != 'value':
+        log_item(itemID, "Body type is not value!")
+        continue
+
+    body_id = ENTITY_PREFIX + body.getTarget().title()
+    if not TEST and body_id not in globes:
+        log_item(itemID, "Unknown globe %s" % body_id)
+        continue
+
+
+    for coord in item.claims[LOCATION]:
+        if coord.getSnakType() != 'value':
+            continue
+        coordValue = coord.getTarget()
+        if coordValue.entity != body_id:
+            # got mislabeled coordinate
+            wb = coordValue.toWikibase()
+            wb['globe'] = body_id
+            newCoord = pywikibot.Coordinate.fromWikibase(wb, site)
+            print("Changed globe for coordinate on %s to %s" % (itemID, body_id))
+            #coord.changeTarget(newCoord)
---
+++ user-config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8  -*-
+
+# This is a sample file. You should use generate_user_files.py
+# to create your user-config.py file.
+
+mylang = 'test'
+family = 'wikidata'
+usernames['wikidata']['test'] = 'LocatorBot'
+usernames['wikidata']['wikidata'] = 'LocatorBot'
+
+console_encoding = 'utf-8'
